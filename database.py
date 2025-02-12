728ed5e7fe42b808b2422092e5846ae8e2b483fd
Antonio Ramirez <antonio.ramirez@globant.com>
2025-02-12 11:08:56 +0100

    CAmbios en database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 # Obtiene la URL de la base de datos desde las variables de entorno
@@ -11,11 +11,11 @@ DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+mysqlconnector://", 1)
 # Crea el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
+# Crear la base declarativa
+Base = declarative_base()
+
 # Sesión de la base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Definir la clase base
-Base = declarative_base()
 
 # Función para obtener la sesión de la base de datos
 def get_db():
