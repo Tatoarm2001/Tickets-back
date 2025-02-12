b03ab03b556c7ef79dacb4f9fbc0dc7a2b94e978
Antonio Ramirez <antonio.ramirez@globant.com>
2025-02-12 11:05:31 +0100

    CAmbios en database.py

--- database.py
+++ database.py
@@ -5,6 +5,9 @@ import os
 # Obtiene la URL de la base de datos desde las variables de entorno
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Reemplaza "mysql://" por "mysql+mysqlconnector://" en la URL
+DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+mysqlconnector://", 1)
+
 # Crea el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
